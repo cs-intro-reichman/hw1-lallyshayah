c5e8c7c1fe180324df4b298de7ab8f7535fb9041
lallyshayah <lally.shayah@post.runi.ac.il>
2024-11-06 18:49:37 +0200

    solution

--- Bill3.java
+++ Bill3.java
@@ -1,27 +1,28 @@
-import java.util.Scanner;
-import java.io.*;
-import java.util.Arrays;
-
-
 // Splits a restaurant bill evenly among three diners.
-public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+public class Bill3 
+{
+	public static void main(String[] args)
+	{
+		
+		String n1 = args[0];
+		String n2 = args[1];
+		String n3 = args[2];
+		int value = Integer.parseInt(args[3]);
+		
+		
+		double newValue;
+		
+		if (value % 3 != 0)
+		{
+			newValue = value / 3 + 1;
+		}
+		
+		else 
+		{
+			newValue = value / 3;
+		}
 
-		Scanner s = new Scanner(System.in);
-
-		String str1 = s.nextLine();
-		String str2 = s.nextLine();
-		String str3 = s.nextLine();
-
-
-		System.out.println("Please enter a number: ");
-		double num = s.nextInt();
-		double Nnum = num / 3;
-		Nnum = Math.ceil(Nnum);
-
-		System.out.println("Dear " + str1 + ", " + str2 + " and " + str3 + ": " + "pay ");
-
+		System.out.println("Dear " + n1 + ", " + n2 + " and " + n3 + ": pay " + newValue + " Shekels each.");
 
 	}
 }
