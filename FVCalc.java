794d7a73d7978d12923c1003324614d2a1d5588b
lallyshayah <lally.shayah@post.runi.ac.il>
2024-11-06 21:12:36 +0200

    new solution

--- FVCalc.java
+++ FVCalc.java
@@ -4,7 +4,7 @@ public class FVCalc
 	public static void main(String[] args)
 	{
 		int currentValue = Integer.parseInt(args[0]);
-		int rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 
 
